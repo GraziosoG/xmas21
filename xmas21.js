e22e0045f91749e904023727da65039e1384fdcf
GraziosoG <grace.chiu25@utexas.edu>
2025-12-18 21:40:33 -0600

    Updated color theme and included responsive design for mobile users

--- xmas21.js
+++ xmas21.js
@@ -14,16 +14,27 @@ function scrollFunction() {
 
 //Pause audio when a new one starts playing
 document.addEventListener('play', function (e) {
-var audios = document.getElementsByTagName('audio');
-for (var i = 0, len = audios.length; i < len; i++) {
-    if (audios[i] != e.target) {
-        audios[i].pause();
+    var audios = document.getElementsByTagName('audio');
+    for (var i = 0, len = audios.length; i < len; i++) {
+        if (audios[i] != e.target) {
+            audios[i].pause();
+        }
     }
-}
 }, true);
 
 function listenAllFunction() {
     var audios = document.getElementsByTagName('audio');
     for (i = 0; i < audios.length; i++) { audios[i].pause() };
     document.getElementById('allsongs').play();
+}
+
+function copyToClipboard() {
+    const url = window.location.href;
+
+    navigator.clipboard.writeText(url).then(() => {
+        // Optional: Provide user feedback
+        alert("Page URL Copied to Clipboard: " + url); 
+    }).catch(err => {
+        console.error('Failed to copy URL: ', err);
+    });
 }
