5eb442b0ced4c7254a368a294af8bbbe9ada08ed
GraziosoG <grace.chiu25@utexas.edu>
2025-12-19 16:54:52 -0600

    Modified visualizations

--- xmas21.js
+++ xmas21.js
@@ -6,19 +6,11 @@ var programPopup = document.getElementById("popup-overlay")
 function showPopup() {
     programButton.style.display = "none";
     programPopup.style.display = "block";
-    var audios = document.getElementsByTagName('audio');
-    for (let i = 0, len = audios.length; i < len; i++) {
-        audios[i].style.zIndex = -1;
-    }
 }
 
 function closePopup() {
     programPopup.style.display = "none";
     programButton.style.display = "block";
-    var audios = document.getElementsByTagName('audio');
-    for (let i = 0, len = audios.length; i < len; i++) {
-        audios[i].style.zIndex = 1;
-    }
 }
 
 //Closes popup if user clicks even if outside of the popup div
@@ -39,14 +31,22 @@ document.addEventListener('play', function (e) {
 }, true);
 
 
+//Play the all piano track from the button link
+function listenAllPiano() {
+    var audios = document.getElementsByTagName('audio');
+    for (i = 0; i < audios.length; i++) { audios[i].pause() };
+    document.getElementById('allsongs').play();
+}
+
+
 //Copy the URL with the section hashtag truncated for users to share
 function copyToClipboard() {
     const url = window.location.href;
 
     navigator.clipboard.writeText(url.split("#")[0]).then(() => {
         // Optional: Provide user feedback
-        alert("Page URL Copied to Clipboard: " + url.split("#")[0]); 
+        alert("網址已複製：" + url.split("#")[0]); 
     }).catch(err => {
-        console.error('Failed to copy URL: ', err);
+        console.error('網址複製發生錯誤！', err);
     });
 }
