ba6b1c8c03b494ee370a62910c560002a8fdf1fe
GraziosoG <grace.chiu25@utexas.edu>
2025-12-19 01:15:37 -0600

    Added pop up menu and css color theme variables

--- xmas21.js
+++ xmas21.js
@@ -1,16 +1,32 @@
-//Back to program button
-var mybutton = document.getElementById("programBtn");
+//Retrieve program button and the overlay background of program div
+var programButton = document.getElementById("program-btn");
+var programPopup = document.getElementById("popup-overlay")
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
+function showPopup() {
+    programButton.style.display = "none";
+    programPopup.style.display = "block";
+    var audios = document.getElementsByTagName('audio');
+    for (let i = 0, len = audios.length; i < len; i++) {
+        audios[i].style.zIndex = -1;
     }
 }
+
+function closePopup() {
+    programPopup.style.display = "none";
+    programButton.style.display = "block";
+    var audios = document.getElementsByTagName('audio');
+    for (let i = 0, len = audios.length; i < len; i++) {
+        audios[i].style.zIndex = 1;
+    }
+}
+
+//Closes popup if user clicks even if outside of the popup div
+window.onclick = function(event) {
+    if (event.target == document.getElementById("popup-overlay")) {
+        closePopup();
+    }
+};
 
 //Pause audio when a new one starts playing
 document.addEventListener('play', function (e) {
@@ -22,18 +38,14 @@ document.addEventListener('play', function (e) {
     }
 }, true);
 
-function listenAllFunction() {
-    var audios = document.getElementsByTagName('audio');
-    for (i = 0; i < audios.length; i++) { audios[i].pause() };
-    document.getElementById('allsongs').play();
-}
 
+//Copy the URL with the section hashtag truncated for users to share
 function copyToClipboard() {
     const url = window.location.href;
 
-    navigator.clipboard.writeText(url).then(() => {
+    navigator.clipboard.writeText(url.split("#")[0]).then(() => {
         // Optional: Provide user feedback
-        alert("Page URL Copied to Clipboard: " + url); 
+        alert("Page URL Copied to Clipboard: " + url.split("#")[0]); 
     }).catch(err => {
         console.error('Failed to copy URL: ', err);
     });
